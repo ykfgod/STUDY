4501fec8591d451986e36ae84ea3b02c535367fa
ykf <ykfgodnew@gmail.com>
2022-02-27 15:13:13 +0300

    ewe TEST

--- com/company/Main.java
+++ com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("TEST");
         System.out.println("TEST2");
+        System.out.println("TEST3");
     }
 }
