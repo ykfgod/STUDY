7e40584ba499f385b1a28aca880b775c8a2af0bf
ykf <ykfgodnew@gmail.com>
2022-02-27 15:50:22 +0300

    Vector class example old synchronized, don't use

---
+++ Collections/ClassVectorExample.java
@@ -0,0 +1,26 @@
+package Collections;
+
+import java.util.Vector;
+
+public class ClassVectorExample {
+
+    public static void main(String[] args) {
+        Vector<String> vector = new Vector<>();
+        vector.add("Yarik");
+        vector.add("Misha");
+        vector.add("Oleg");
+        vector.add("Katya");
+
+        System.out.println(vector);
+
+        System.out.println(vector.firstElement());
+
+        System.out.println(vector.lastElement());
+
+        vector.remove(2);
+        System.out.println(vector);
+
+        System.out.println(vector.get(1));
+
+    }
+}
--- com/company/Main.java
+++ com/company/Main.java
@@ -3,8 +3,5 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("TEST");
-        System.out.println("TEST2");
-        System.out.println("TEST3");
     }
 }
