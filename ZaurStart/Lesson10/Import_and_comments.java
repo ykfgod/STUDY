383d10a3889066322a8df9a5c7c9c5b25042f293
ykf <ykfgodnew@gmail.com>
2022-03-07 18:04:30 +0300

    9 and 10 lessons, empty classes, read in them infomation

---
+++ ZaurStart/Lesson10/Import_and_comments.java
@@ -0,0 +1,9 @@
+package ZaurStart.Lesson10;
+
+/*
+    Простой урок о ключевом слове import, import static и комментариях
+    Смотри видео
+ */
+
+public class Import_and_comments {
+}
---
+++ ZaurStart/Lesson9/JustWatchLesson.java
@@ -0,0 +1,9 @@
+package ZaurStart.Lesson9;
+
+/*
+    Для понимания работы ключевого слова this, область видимости переменных и garbege collector
+    смотри урок 9
+ */
+
+public class JustWatchLesson {
+}
